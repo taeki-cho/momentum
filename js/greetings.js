39b2e928c7442e594a2d21ee8b22176dcba8d651
taeki-cho <taegi81@gmail.com>
2022-04-20 12:53:27 +0900

    수정

---
+++ js/greetings.js
@@ -0,0 +1,11 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButtom = loginForm.querySelector("button");
+
+// const loginInput = document.querySelector("#login-form input");
+// const loginButtom = document.querySelector("#login-form button");
+
+function handleLoginBtnClick(){
+
+}
+loginButtom.addEventListener("click", handleLoginBtnClick);
